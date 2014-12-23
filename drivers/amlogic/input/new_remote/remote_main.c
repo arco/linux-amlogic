a62c91a6595f17d892b7d00f9e9bb1c8f8e03b86
Mauro Ribeiro <mauro.ribeiro@hardkernel.com>
2014-12-23 01:12:04 -0200

    amlogic:new_remote: silence the pressed events
    Change-Id: I0373cef656d0e7f5b3556c395f4ccb4c4efd020a

--- drivers/amlogic/input/new_remote/remote_main.c
+++ drivers/amlogic/input/new_remote/remote_main.c
@@ -222,7 +222,6 @@ static int remote_mouse_event(struct input_dev *dev, unsigned int scancode, unsi
 
 void remote_send_key(struct input_dev *dev, unsigned int scancode, unsigned int type,int event)
 {
-	printk("remote_send_key\n");
 	if(scancode == FN_KEY_SCANCODE && type == 1) {
 		// switch from key to pointer
 		if(key_pointer_switch) {
@@ -276,7 +275,6 @@ void remote_send_key(struct input_dev *dev, unsigned int scancode, unsigned int 
 				input_dbg("repeat ircode = 0x%02x, scancode = 0x%04x, maptable = %d \n", scancode, key_map[gp_remote->map_num][scancode],gp_remote->map_num);
 				break;
 		}
-		input_dbg("%s sleep:%d\n", __func__, gp_remote->sleep);
 		if(gp_remote->sleep && scancode == 0x1a && key_map[gp_remote->map_num][scancode] == 0x0074) {
 			printk(" set AO_RTI_STATUS_REG2 0x4853ffff \n");
 			WRITE_AOBUS_REG(AO_RTI_STATUS_REG2, 0x4853ffff); // tell uboot don't suspend
