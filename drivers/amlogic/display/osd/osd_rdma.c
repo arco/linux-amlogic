ef5274ed6b241eb648ff042d1cd2ebdabe9021fd
Peifu Jiang <peifu.jiang@amlogic.com>
2015-04-21 13:04:35 +0800

    PD#105323(105111): osd: add lock for rdma item table
    Change-Id: I9bd9ba8458e80a99a0ed0f12aca32b55c17e58ff
    
    Conflicts:
    
    	drivers/amlogic/display/osd/osd_rdma.c
    
    Former-commit-id: edb21d4a6e40727a83559f920bbec3360d3cbd6d

--- drivers/amlogic/display/osd/osd_rdma.c
+++ drivers/amlogic/display/osd/osd_rdma.c
@@ -25,6 +25,11 @@
  */
 #include "osd_rdma.h"
 #include <linux/amlogic/amlog.h>
+#include <linux/delay.h>
+#include <linux/spinlock.h>
+
+static DEFINE_MUTEX(rdma_mutex);
+static DEFINE_SPINLOCK(rdma_lock);
 
 static rdma_table_item_t *rdma_table = NULL;
 static u32		   table_paddr = 0;
@@ -57,14 +62,19 @@ void osd_rdma_start(void);
 
 static void inline reset_rdma_table(void)
 {
+	unsigned long flags;
+
+	spin_lock_irqsave(&rdma_lock, flags);
 	aml_write_reg32(END_ADDR, table_paddr - 1);
 	rdma_table[0].addr = OSD_RDMA_FLAG_REG;
 	rdma_table[0].val = OSD_RDMA_STATUS_MARK_TBL_RST;
 	item_count = 1;
+	spin_unlock_irqrestore(&rdma_lock, flags);
 }
 
 static int update_table_item(u32 addr, u32 val)
 {
+	unsigned long flags;
 	int retry_count = OSD_RDMA_UPDATE_RETRY_COUNT;
 retry:
 	//in reject region then we wait for hw rdma operation complete.
@@ -79,17 +89,21 @@ retry:
 	//atom_lock_start:
 	//set write op aotmic lock flag.
 	OSD_RDMA_STAUS_MARK_DIRTY;
+	spin_lock_irqsave(&rdma_lock, flags);
 	item_count++;
 	rdma_table[item_count].addr = OSD_RDMA_FLAG_REG;
 	rdma_table[item_count].val = OSD_RDMA_STATUS_MARK_COMPLETE;
 	aml_write_reg32(END_ADDR, (table_paddr + item_count * 8 + 7));
 	rdma_table[item_count - 1].addr = addr;
 	rdma_table[item_count - 1].val = val;
+	spin_unlock_irqrestore(&rdma_lock, flags);
 	//if dirty flag is cleared, then RDMA hw write and cpu sw write is racing.
 	//if reject flag is true,then hw RDMA hw write start when cpu write.
 	//atom_lock_end:
 	if (!OSD_RDMA_STAUS_IS_DIRTY || OSD_RDMA_STATUS_IS_REJECT) {
+		spin_lock_irqsave(&rdma_lock, flags);
 		item_count--;
+		spin_unlock_irqrestore(&rdma_lock, flags);
 		goto retry ;
 	}
 	return 0;
